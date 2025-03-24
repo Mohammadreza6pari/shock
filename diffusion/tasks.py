21dca89979606262d890e88b9aacb87e4bc910e0
mr.sheshpari <mohammad6pari@gmail.com>
2025-03-24 18:19:41 +0330

    error in diffusion task running added

--- diffusion/tasks.py
+++ diffusion/tasks.py
@@ -5,11 +5,34 @@ def run_diffusion(diffusion_id):
     try:
         diffusion = Diffusion.objects.get(id=diffusion_id)
         diffusion.create_output_folder()
-        subprocess.run(diffusion.cmd_arguments, shell=False, check=True)
+        result = subprocess.run(
+            diffusion.cmd_arguments,
+            shell=False, 
+            text=True,
+            capture_output=True,
+            check=True
+            )
+        
+        if Diffusion.SUCCESS_CMD_OUTPUT not in result.stdout or result.stderr:
+            raise Exception(result.stderr or result.stdout)
+        
+        diffusion.logs = {
+            "stdout": result.stdout,
+        }
+
         diffusion.status = 'finished'
+    
+    except subprocess.CalledProcessError as err:
+        diffusion.logs = {
+            "error": str(err.stderr),
+        }
+        diffusion.status = 'failed'
     
     except Exception as err:
         print(f"Error occuured in run_diffusion task. {repr(err)}")
+        diffusion.logs = {
+            "error": str(err)
+        }
         diffusion.status = 'failed'
         raise Exception("Error in run_diffusion")
     
