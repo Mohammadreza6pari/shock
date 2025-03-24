ad48bfc76e5b8dc5ae845c9aafca836e7385c1d2
mr.sheshpari <mohammad6pari@gmail.com>
2025-03-24 16:30:04 +0330

    diffusion running added

--- diffusion/models.py
+++ diffusion/models.py
@@ -1,10 +1,12 @@
 from django.db import models
-import subprocess
 from django.contrib.postgres.fields import ArrayField
-
+import os 
+import threading
 
 class Diffusion(models.Model):
-    RESULT_BASE_PATH = './ShockOutputs/'
+    RESULT_BASE_PATH = './diffusion/outputs/'
+    EXECUTABLE_JAR_FILE_PATH = './diffusion/diffusion_files/diffusion_version3.jar'
+    BASE_INTEGRATION_FILES_PATH = './diffusion/diffusion_files/'
 
 
     STATUS_CHOICES = (
@@ -12,8 +14,9 @@ class Diffusion(models.Model):
         ('running', 'Running'),
         ('finished', 'Finished'),
         ('failed', 'Failed'),
-    )
 
+    )
+    id = models.AutoField(primary_key=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=12, choices=STATUS_CHOICES, default='pending')
@@ -21,40 +24,79 @@ class Diffusion(models.Model):
     number_of_iterations = models.PositiveIntegerField() 
     integration = models.CharField(max_length=36) #TODO: That's better it maps to integration model
     sources = ArrayField(
-        models.CharField(max_length=12),
+        models.CharField(max_length=12, blank=True, null=True),
     )
     destinations = ArrayField(
-        models.CharField(max_length=12),
+        models.CharField(max_length=12, blank=True, null=True),
     )
     shock_types = ArrayField(
-        models.CharField(max_length=12),
+        models.CharField(max_length=12, blank=True, null=True),
     )
     shock_amounts = ArrayField(
-        models.CharField(max_length=12),
+        models.CharField(max_length=12, blank=True, null=True),
     )
-    threshold_one = models.IntegerField()
-    threshold_two = models.IntegerField()
-    threshold_three = models.IntegerField()
+    threshold_one = models.FloatField()
+    threshold_two = models.FloatField()
+    threshold_three = models.FloatField()
     
     def __str__(self):
-        return f"Diffusion {self.unique_id} - {self.status}"
+        return f"Diffusion {self.id} - {self.status}"
     
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        
+        if self.status == 'pending':
+            self.run_diffusion_async()
 
     @property
     def output_file_name(self):
-        return self.id
+        return str(self.id) + "/"
     
     @property
-    def output_file_path(self):
+    def output_folder_path(self):
         return Diffusion.RESULT_BASE_PATH + self.output_file_name
+
+    @property
+    def output_folder_path_exists(self):
+        return os.path.exists(self.output_folder_path) and os.path.isdir(self.output_folder_path)
+
+    @property
+    def output_integration_folder_path(self):
+        return os.path.join(Diffusion.BASE_INTEGRATION_FILES_PATH, f"{self.integration}.csv")
+            
+    def create_output_folder(self):
+        if self.output_folder_path_exists:
+            raise Exception("floder exist")
+        os.makedirs(self.output_folder_path, exist_ok=True)
+        return self.output_folder_path
     
-    def _diffusion_command(self):
-        ordered_command_list = list()
-        ordered_command_list.append('java')
-        ordered_command_list.append('-jar')
-        ordered_command_list.append(self.output_file_path)
+    @staticmethod
+    def to_cmd_argument_fields(fields):
+        return ":".join(fields)
+    
+    @property
+    def cmd_arguments(self):
+        command_args = [
+            'java', '-jar', 
+            Diffusion.EXECUTABLE_JAR_FILE_PATH,
+            self.output_integration_folder_path,  # Integration CSV file path
+            self.output_folder_path,  # Result path for output
+            Diffusion.to_cmd_argument_fields(self.sources),  # Sources as a colon-separated string
+            Diffusion.to_cmd_argument_fields(self.destinations),  # Destinations as a colon-separated string
+            Diffusion.to_cmd_argument_fields(self.shock_types),  # Shock types as a colon-separated string
+            Diffusion.to_cmd_argument_fields(self.shock_amounts),  # Shock amounts as a colon-separated string
+            str(self.number_of_iterations),  # Iterations
+            str(self.threshold_one),  # Threshold 1
+            str(self.threshold_two),  # Threshold 2
+            str(self.threshold_three)# Threshold 3
+        ]
+        return command_args
+    
+    def run_diffusion_async(self):
+        from .tasks import run_diffusion
 
-        return ordered_command_list 
-
-    def run_diffusion(self):
-        subprocess.call(self._diffusion_command, shell=True)
+        if self.status == 'pending':
+            self.status = 'running'
+            self.save(update_fields=['status'])
+            thread = threading.Thread(target=run_diffusion, args=(self.id,))
+            thread.start()
