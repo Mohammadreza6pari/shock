6e09a807c7034e2e4e71699f84b4bf8d96437012
mr.sheshpari <mohammad6pari@gmail.com>
2025-03-24 17:30:51 +0330

    path api modified

--- shock/urls.py
+++ shock/urls.py
@@ -5,6 +5,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('diffusion.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
