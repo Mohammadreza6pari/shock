f5c4d9b3e64dd773c5f120dffcbeb74f952fb626
mr-sheshpari <mohammad6pari@gmail.com>
2025-09-12 15:04:23 +0330

    get user profile view added

--- user/views.py
+++ user/views.py
@@ -2,6 +2,10 @@ from rest_framework import generics
 from rest_framework.permissions import AllowAny
 from .serializers import RegisterSerializer
 from django.contrib.auth import get_user_model
+from rest_framework.views import APIView 
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
 
 User = get_user_model()
 
@@ -9,3 +13,16 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
     permission_classes = [AllowAny]
+
+class UserProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        return Response({
+            "id": user.id,
+            "username": user.username,
+            "email": user.email,
+            "is_approved": user.is_approved,
+            "is_active": user.is_active
+        })
